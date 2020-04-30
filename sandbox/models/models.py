b62cbea51c34d044b601b256a3da572cc8a7f11a
BabakMahmoudi <babak@gnco.ir>
2020-04-30 17:17:10 +0430

    sandbox added

---
+++ sandbox/models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class sandbox(models.Model):
+#     _name = 'sandbox.sandbox'
+#     _description = 'sandbox.sandbox'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
