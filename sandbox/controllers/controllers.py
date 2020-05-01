16c7f1e8247e6dbbf4ff7edd510c4e80db887d51
BabakMahmoudi <babak@gnco.ir>
2020-05-01 14:48:51 +0430

    sandbox uncommented

--- sandbox/controllers/controllers.py
+++ sandbox/controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Sandbox(http.Controller):
-#     @http.route('/sandbox/sandbox/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Sandbox(http.Controller):
+    @http.route('/sandbox/sandbox/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/sandbox/sandbox/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('sandbox.listing', {
-#             'root': '/sandbox/sandbox',
-#             'objects': http.request.env['sandbox.sandbox'].search([]),
-#         })
+    @http.route('/sandbox/sandbox/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('sandbox.listing', {
+            'root': '/sandbox/sandbox',
+            'objects': http.request.env['sandbox.sandbox'].search([]),
+        })
 
-#     @http.route('/sandbox/sandbox/objects/<model("sandbox.sandbox"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('sandbox.object', {
-#             'object': obj
-#         })
+    @http.route('/sandbox/sandbox/objects/<model("sandbox.sandbox"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('sandbox.object', {
+            'object': obj
+        })
--- sandbox/models/models.py
+++ sandbox/models/models.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class sandbox(models.Model):
-#     _name = 'sandbox.sandbox'
-#     _description = 'sandbox.sandbox'
+class sandbox(models.Model):
+    _name = 'sandbox.sandbox'
+    _description = 'sandbox.sandbox'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+    def generate_record_name(self):
+        print("hi there")
+        
